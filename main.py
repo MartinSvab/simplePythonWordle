30d9e59bc3eaccc7bd348d015acd8045e48ec55e
Martin Svab <1105@student.itgymnazium.cz>
2023-12-18 10:09:58 +0100

    yipiie

--- main.py
+++ main.py
@@ -5,22 +5,29 @@ import time
 import json
 
 
-#Other functions
+# Other functions
 def cls():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
+def startGame():
+    print("Fetching words...")
+    data = getDataFromWeb('https://random-word-api.herokuapp.com/all?lang=en')
+    data.pop(0)
+    data.pop(len(data) - 1)
+    print("Words fetched successfully!\nStarting game!")
+    time.sleep(0.5)
+    cls()
+    return data
 
 
-
-
-#Functions used in main cycle
+# Functions used in main cycle
 
 def guessWrong(guess, word, correctlyGuessed):
     for letterInGuess in guess:
         if letterInGuess in word:
             for index, val in enumerate(word):
-                if(val == letterInGuess):
+                if (val == letterInGuess):
                     correctlyGuessed[index] = letterInGuess
         else:
             print(letterInGuess + " is not in the word!")
@@ -29,46 +36,34 @@ def guessWrong(guess, word, correctlyGuessed):
 
 
 def getGuess():
-    while(True):
+    while (True):
         guess = input()
         if guess in data:
             return guess
-        else: print("This word is not in the list. Try again")
+        else:
+            printSpaces(2)
+            print("This word is not in the list. Try again")
+
 
 
 def getDataFromWeb(url):
     response = urlopen(url)
     return response.read().decode('utf-8').split("\",\"")
 
-def findInDictionary(word):
-    dictionary = getDataFromWeb("https://api.dictionaryapi.dev/api/v2/entries/en/hello")
 
-    if isinstance(dictionary, list) and dictionary:
-        search_string = "abreacted"
-
-        try:
-            index = [entry.get("word", "").lower() for entry in dictionary].index(search_string.lower())
-            print(f"Index of '{search_string}': {index}")
-        except ValueError:
-            print(f"'{search_string}' not found in the list.")
-    else:
-        print("Invalid or empty data returned by the API.")
+def printSpaces(numberOfSpaces):
+    for i in range(numberOfSpaces):
+        print(" ")
 
 
-#Getting data from internet, starting game
+# Getting data from internet, starting game
 
-print("Fetching words...")
-data = getDataFromWeb('https://random-word-api.herokuapp.com/all?lang=en')
-data.pop(0)
-data.pop(len(data)-1)
-print("Words fetched successfully!\nStarting game!")
-time.sleep(0.5)
-cls()
+data = startGame()
 
+# main cycle
 
-#main cycle
-
-while(False):
+while True:
+    printSpaces(2)
     word = random.choice(data)
     correctlyGuessed = []
 
@@ -78,16 +73,16 @@ while(False):
 
     print("\nGuess!")
 
-    while(True):
+    while True:
         guess = getGuess()
 
-        if(guess == word):
+        if guess == word:
             print("Correct!")
+            printSpaces(3)
             break
 
-        else: guessWrong(guess, word, correctlyGuessed)
+        else:
+            guessWrong(guess, word, correctlyGuessed)
 
         if "_" not in correctlyGuessed:
             print("\n\nYOU WIN")
-
-findInDictionary("random")
